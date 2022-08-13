89c5a0361020974e121e98e219fa80960c030b6b
Boula Akladyous <akladyous@gmail.com>
2022-08-13 13:42:11 -0400

    added completedController

---
+++ app/controllers/wizards/listings/completed_controller.rb
@@ -0,0 +1,11 @@
+module Wizards
+  module Listings
+    class CompletedController < ApplicationController
+
+      def show
+
+      end
+
+    end
+  end
+end
---
+++ app/controllers/wizards/listings/pet_controller.rb
@@ -0,0 +1,14 @@
+module Wizards
+  module Listings
+    class PetController < ApplicationController
+
+      def new
+      end
+
+      def create
+
+      end
+
+    end
+  end
+end
