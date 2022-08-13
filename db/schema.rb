e83592b3fa4bfbc587e65c8241289d675a35a381
Boula Akladyous <akladyous@gmail.com>
2022-08-13 13:13:37 -0400

    generated db migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_08_12_200731) do
+ActiveRecord::Schema[7.0].define(version: 2022_08_13_170742) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -42,6 +42,39 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_200731) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.integer "type"
+    t.datetime "lost_found_date"
+    t.string "address"
+    t.string "crossroads"
+    t.text "pet_description"
+    t.text "comment"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
+  create_table "pets", force: :cascade do |t|
+    t.string "name"
+    t.integer "species"
+    t.string "breed"
+    t.integer "gender"
+    t.string "color"
+    t.string "coat"
+    t.integer "size"
+    t.decimal "age", precision: 4, scale: 2
+    t.decimal "height", precision: 5, scale: 2
+    t.decimal "weight", precision: 5, scale: 2
+    t.integer "microchip"
+    t.boolean "collar"
+    t.text "description"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_pets_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -61,4 +94,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_08_12_200731) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "listings", "users"
+  add_foreign_key "pets", "users"
 end
