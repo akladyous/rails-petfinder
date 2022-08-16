7bd0742d236e1cedee5750a3c14793498151da8f
Boula Akladyous <akladyous@gmail.com>
2022-08-16 18:32:03 -0400

    removed wizard namespace from listing routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,9 @@
 Rails.application.routes.draw do
-  # namespace :wizards do
     namespace :listings do
       resources :reports, only: [:new, :create]
       resources :pets, only: [:new, :create]
       resources :completed, only: :show
     end
-  # end
 
   devise_for :users
   root "home#index"
