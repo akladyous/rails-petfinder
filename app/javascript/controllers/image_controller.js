98db9a0d75c2bc05ffbadcd2d68a08c69f081761
Boula Akladyous <akladyous@gmail.com>
2022-08-16 18:28:15 -0400

    added

--- app/javascript/controllers/image_controller.js
+++ app/javascript/controllers/image_controller.js
@@ -5,5 +5,17 @@ export default class extends Controller {
     static targets = ["imageContainer", "input", "output"]
     connect() {
     }
+    handleChange(event) {
+        let input = this.inputTarget;
+        let output = this.outputTarget;
 
+        let fileReader = new FileReader();
+        if (input.files[0]) {
+            fileReader.onload = () => {
+                output.src = fileReader.result;
+            };
+        }
+        // input.parentElement.insertBefore(imageTag, input.nextSibling)
+        fileReader.readAsDataURL(input.files[0])
+    };
 }
