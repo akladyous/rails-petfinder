6385360f9ab5d60c99893b058041d34f5b7eee15
Boula Akladyous <akladyous@gmail.com>
2022-08-15 21:06:38 -0400

    added image upload tags

--- app/models/listings/pet_info.rb
+++ app/models/listings/pet_info.rb
@@ -2,7 +2,7 @@ module Listings
   class PetInfo
     include ActiveModel::Model
 
-    attr_accessor :name, :species, :gender, :size, :breed, :color, :coat, :age, :height, :weight, :microchip, :collar, :description
+    attr_accessor :image, :name, :species, :gender, :size, :breed, :color, :coat, :age, :height, :weight, :microchip, :collar, :description
 
     validates :name, :species, :gender, :size, presence: true
 
