53a49537c509cd7105dcf5cfc0625afd19aac4d6
Boula Akladyous <akladyous@gmail.com>
2022-08-17 17:10:27 -0400

    created db seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+Faker::Config.locale = :en
+
+system('clear')
+puts "How many records ?"
+total_record = gets.strip().to_i
+puts "🌱 Seeding #{total_record.to_i} Users ..."
+
+spinner = Enumerator.new do |e|
+    loop do
+        e.yield '|'
+        e.yield '/'
+        e.yield '-'
+        e.yield '\\'
+    end
+end
+
+
