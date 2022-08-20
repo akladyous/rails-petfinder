11ae86af7c84f8b691b87cd9cff2b16f78887c0e
Boula Akladyous <akladyous@gmail.com>
2022-08-20 13:17:39 -0400

    create search box

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,17 @@
 module ApplicationHelper
+  def feedback_for?(object, attribute)
+    # debugger
+    return nil if object.errors.empty?
 
-    def feedback_for?(object, attribute)
-      # debugger
-        return nil if object.errors.empty?
-        if object.errors.has_key?(attribute)
-            return content_tag :div, nil, { class: ['d-block', 'invalid-feedback'] } do
-                object.errors.full_messages_for(attribute).to_sentence
-            end
-        end
-        nil
+    if object.errors.has_key?(attribute)
+      return content_tag :div, nil, { class: %w[d-block invalid-feedback] } do
+        object.errors.full_messages_for(attribute).to_sentence
+      end
     end
+    nil
+  end
 
-    def input_selector(model, attribute)
-      model.public_send(attribute.pluralize.to_sym).map { |k,_| [k.capitalize, k] }
-    end
-
+  def input_selector(model, attribute)
+    model.public_send(attribute.pluralize.to_sym).map { |k, _| [k.capitalize, k] }
+  end
 end
